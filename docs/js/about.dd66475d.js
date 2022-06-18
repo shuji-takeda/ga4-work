389eb5df554fd769871777fc91b98f793b22a2bc
shuji-takeda <12.hack.17.takeshu@gmail.com>
2022-06-18 14:52:48 +0900

    buiild

--- docs/js/about.dd66475d.js
+++ docs/js/about.dd66475d.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkvue3"]=self["webpackChunkvue3"]||[]).push([[443],{242:function(n,u,e){e.r(u),e.d(u,{default:function(){return l}});var t=e(252);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=e(744);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
-//# sourceMappingURL=about.47c3ae2e.js.map
+//# sourceMappingURL=about.dd66475d.js.map

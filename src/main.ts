8782340b6399825c716347b3f5d9cb275b04ce86
shuji-takeda <12.hack.17.takeshu@gmail.com>
2022-06-18 14:33:05 +0900

    vue3

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueGtag, {
+    config: {
+      id: process.env.VUE_APP_GA_TRACKING_ID,
+    },
+  })
+  .mount("#app");
